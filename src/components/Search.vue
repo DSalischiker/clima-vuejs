646fcbfe37ede28788422a34803699d4f5213490
Diego <diegosalischiker711@gmail.com>
2021-05-13 20:58:42 -0300

    Search Component
    - En método callAPI() se hace un $emit de loadingData para avisar que se está trayendo la información para modificar html con un loading (en Data.vue).
    - Agregado un setTimeout para simular tiempo de carga para mostrar el loading.
    - Agregado keymonitor para disparar el submit al apretar enter estando el focus en el input.

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>
-    <input v-model="cityInput" type="text" name="" id="" />
-    <button type="submit" @click="callAPI">Buscar</button>
+  <div class="search-container">
+    <input
+      @keyup.prevent="keymonitor"
+      v-model="cityInput"
+      placeholder="Ingrese una ciudad"
+      type="text"
+      name="Search"
+    />
+    <button type="submit" @click="callAPI">
+      <i class="ri-search-line"></i>
+    </button>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import "remixicon/fonts/remixicon.css";
 export default {
   name: "Search",
   data() {
@@ -18,18 +27,29 @@ export default {
   },
   computed: {
     getURL() {
-      return "" + this.BASE_URL + this.cityInput + "&units=metric&appid=" + this.apiKey;
+      return `${this.BASE_URL}${this.cityInput}&units=metric&appid=${this.apiKey}`;
     },
   },
   methods: {
     callAPI() {
-      axios.get(this.getURL)
+      this.$emit("loadingData");
+      axios
+        .get(this.getURL)
         .then((response) => {
           console.log(response.data);
+          //timeOut para simular loading
+
+          setTimeout(() => {  this.$emit("showData", response.data); }, 1500);
         })
         .catch((error) => console.error(error))
         .then(() => console.log("hice el get"));
-      this.cityInput = '';
+      /* this.cityInput = ''; */
+    },
+    keymonitor: function (event) {
+      console.log(event.key);
+      if (event.key == "Enter") {
+        this.callAPI();
+      }
     },
   },
 };
