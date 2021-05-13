1a797b486b79138febd5b80b4ef086b77a2ef7f8
Diego <diegosalischiker711@gmail.com>
2021-05-13 16:40:58 -0300

    Componente Weather + Data + Computed + Methods
    Componente padre Weather que es donde está el funcionamiento.
    
    Llama a componentes Search (input de búsqueda que consulta API de OpenWeatherMap via axios.
    Y a componente Data que muestra weatherData (enviada por props).
    
    Data:
    weatherData -> donde se guarda la response de la API.
    loadingState -> true si está buscando data de la API.
    isHot -> true si la temperatura actual supera los 18°C
    
    Computed:
    currentDate -> Devuelve fecha actual.
    itsDayTime -> Devuelve si es de día o de noche
    
    Methods:
    getHour -> trae hora del día
    showData -> funciona disparada por $emit en <Search/>
    loadingData -> disparada por emit en <Search/> para saber si está cargando

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,19 +1,58 @@
 <template>
-  <div>
-    <h1>Clima</h1>
-    <Search />
-    <Data />
+  <div class="weather-container" :class="[itsDayTime ? 'day' : 'night', isHot ? 'hot' : 'cold']">
+    <div class="components-container">
+      <p class="date">{{ currentDate }}</p>
+      <Search class="search" @showData="showData" @loadingData="loadingData"  />
+      <Data
+        class="data"
+        :weatherData="weatherData"
+        :loadingData="loadingState"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import Search from "@/components/Search.vue";
 import Data from "@/components/Data.vue";
+import moment from "moment";
 export default {
-  name: "Clima",
+  name: "Weather",
   components: {
     Search,
     Data,
+  },
+  data() {
+    return {
+      weatherData: {},
+      loadingState: false,
+      isHot: null,
+    };
+  },
+  computed: {
+    currentDate() {
+      return moment().format("dddd Do MMMM");
+    },
+    itsDayTime() {
+      return this.getHour() < "14:00";
+    },
+  },
+  methods: {
+    getHour(){
+      const today = new Date();
+      const time =
+        today.getHours() + ":" + today.getMinutes();
+        return time;
+    },
+    showData(data) {
+      this.weatherData = data;
+      this.loadingState = false;
+      this.isHot = this.weatherData.main.temp >= 18;
+    },
+    loadingData() {
+      this.loadingState = true;
+      console.log("loadingtrue");
+    },
   },
 };
 </script>
