f1783bf4939639fd04612f0831948c5906f8aa74
Diego <diegosalischiker711@gmail.com>
2021-05-13 21:07:13 -0300

    Estilos en App.vue

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,21 @@ export default {
 </script>
 
 <style lang="scss">
+body{
+background: #252525;
+width: 100%;
+height: 100vh;
+margin: 0;
+padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  height: 100vh;
+  display: flex;
+  align-items: center;
 }
 </style>
